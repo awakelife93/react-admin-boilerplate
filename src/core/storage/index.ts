e6c88cb2c9a02bdadb4b80127796e45945a022e8
harry <awakelife93@gmail.com>
2021-11-04 23:53:35 +0900

    storage module 확장

--- src/core/storage/index.ts
+++ src/core/storage/index.ts
@@ -1,13 +1,35 @@
+import _ from "lodash";
+
 export const getLocalStorageItem = (key: string): string | null =>
   window.localStorage.getItem(key);
 
+export const getLocalStorageItems = (keys: string[]): string[] => {
+  let results: string[] = [];
+
+  _.forEach(keys, (key: string) => {
+    const result = getLocalStorageItem(key);
+
+    if (result !== null) {
+      results.push(result);
+    }
+  });
+
+  return results;
+};
+
 export const setLocalStorageItem = (item: any): void => {
-  Object.keys(item).forEach((key) => {
+  _.forEach(Object.keys(item), (key: string) => {
     window.localStorage.setItem(key, item[key]);
   });
 };
 
 export const removeLocalStorageItem = (key: string): void =>
   window.localStorage.removeItem(key);
+
+export const removeLocalStorageItems = (keys: string[]): void => {
+  _.forEach(keys, (key: string) => {
+    removeLocalStorageItem(key);
+  });
+};
 
 export const clearLocalStorageItem = (): void => window.localStorage.clear();
