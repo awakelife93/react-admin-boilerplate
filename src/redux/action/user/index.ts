7ccbe73e92020936e41473465812c2e1ae3f36cc
hw.park <hw.park@fastfive.co.kr>
2022-02-14 12:48:22 +0900

    버그 수정

--- src/redux/action/user/index.ts
+++ src/redux/action/user/index.ts
@@ -1,12 +1,12 @@
 import { initUserState } from "../../reducer/user/default";
-import { ActionEnum } from "../../type";
+import { ActionEnum, UserStoreType } from "../../type";
 
 export const initUserInfoAction = () => ({
   type: ActionEnum.SET_USER_INFO,
   value: initUserState.user,
 });
 
-export const setUserInfoAction = (value: boolean) => ({
+export const setUserInfoAction = (value: UserStoreType) => ({
   type: ActionEnum.SET_USER_INFO,
   value,
 });
